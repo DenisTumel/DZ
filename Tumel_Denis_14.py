b24ee6c28e2abf3f0d1f2f43dce3e34eb992db3b
DenisTumel <132933190+DenisTumel@users.noreply.github.com>
2023-05-12 22:50:04 +0300

    Add files via upload

--- Tumel_Denis_14.py
+++ Tumel_Denis_14.py
@@ -3,25 +3,18 @@
 # Обработать ошибку: “Деление на ноль”
 # Ноль использовать в качестве завершения программы (сделать как отдельную операцию).
 # Работу загружаем в репозиторий гита. Скидываем ссылку НА ФАЙЛ!
+def addition(a, b):return a + b
+def subtraction(a, b): return a - b
+def multiplication(a, b): return a * b
+def division(a, b): return a / b
+def division_by_zero(a, b):return ('На ноль делить нельзя!')
+
 while True:
-    sign=str(input('Введите операцию (для выхода введите 0): '))
-    if sign== '0':
+    sign = str(input('Введите операцию (для выхода введите 0): '))
+    if sign == '0':
         break
     a = float(input('Введите первое число: '))
-    b= float(input('Введите второе число: '))
-    def addition(a, b):
-        return a + b
-    def subtraction(a, b):
-        return a - b
-    def multiplication(a, b):
-        return a * b
-    def division(a, b):
-        return a / b
-    def division_by_zero(a, b):
-        return ('На ноль делить нельзя!')
-
-
-
+    b = float(input('Введите второе число: '))
     if b=='0':print(division_by_zero)
     if sign=='+': print(addition(a,b))
     elif sign=='-':print(subtraction(a,b))
